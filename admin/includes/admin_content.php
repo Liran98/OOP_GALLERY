b99daf84129f5859a179da23cbdc7d21fcc2a35f
liran <liran9800@gmail.com>
2024-04-17 22:35:14 +0300

    new mysqli class

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -1,30 +1,31 @@
 <div class="container-fluid">
 
-        
+    <?php
+    $res = $database->query("SELECT * FROM users WHERE id = 1");
+
+   while ($row = mysqli_fetch_assoc($res)){
+    echo $row['id'];
+   }
+?>
+
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">
-                    ADMIN
+                    ADMIN 
                     <small>Subheading</small>
                 </h1>
-
-<?php
-if($database->conn){
-    echo "true";
-}
-
-?>
+         
 
 
 
-                <ol class="breadcrumb">
-                    <li>
-                        <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
-                    </li>
-                    <li class="active">
-                        <i class="fa fa-file"></i> Blank Page
-                    </li>
-                </ol>
+            <ol class="breadcrumb">
+                <li>
+                    <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
+                </li>
+                <li class="active">
+                    <i class="fa fa-file"></i> Blank Page
+                </li>
+            </ol>
             </div>
-        </div> 
+        </div>
         <!-- /.row -->
--- admin/includes/database.php
+++ admin/includes/database.php
@@ -9,13 +9,46 @@ class Database
     {
         $this->open_db_connection();
     }
-  //new comment
+    //new comment
 
     public function open_db_connection()
     {
-        $this->conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-       
+        // $this->conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+        //same as mysqli_errno($this->conn);
+        if($this->conn->connect_errno){
+            die($this->conn->connect_error);
+        }
     }
-}
+
+    public function query($sql)
+    {
+        // $results = mysqli_query($this->conn, $sql);
+        $results = $this->conn->query($sql);
+
+        $this->confirm_query($results);
+        return $results;
+    }
+
+    private function confirm_query($result)
+    {
+        if (!$result) {
+            die(mysqli_error($this->conn->error));
+        }
+    }
+
+    public function escape_string($string)
+    {
+        // $escaped_string =  mysqli_real_escape_string($this->conn, $string);
+        $escaped_string = $this->conn->real_escape_string($string);
+        return $escaped_string;
+    }
+
+    public function the_insert_id(){
+        // return mysqli_insert_id($this->conn);
+        return $this->conn->insert_id;
+    }
+}//end of class 
 
 $database = new Database();
